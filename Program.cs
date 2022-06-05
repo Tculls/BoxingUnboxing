8aec4bda8b81aa00dc863b2a7f82b0b4647e21c4
Tculls <tanner.culley@gmail.com>
2022-06-05 11:56:18 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace BoxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> stuffs = new List<object>();
+            stuffs.Add(7);
+            stuffs.Add(28);
+            stuffs.Add(-1);
+            stuffs.Add(true);
+            stuffs.Add("chair");
+
+            int sum = 0;
+
+            foreach (var stuff in stuffs)
+            {
+                Console.WriteLine(stuff);
+
+                if (stuff is int)
+                {
+                    sum += (int)stuff;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
